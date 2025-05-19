1c139ee664df8ac5fff87a64c96db3dfec0655ce
souravMitra02 <inf.souravmitra@gmail.com>
2025-05-19 17:21:23 +0600

    added user in database

--- index.js
+++ index.js
@@ -30,6 +30,7 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
       await client.connect();
       const coffeeCollection = client.db('coffeeDB').collection('coffees');
+      const userCollection = client.db('coffeeDB').collection('users');
 
       app.get('/coffees', async (req, res) => {
           const result = await coffeeCollection.find().toArray();
@@ -70,6 +71,29 @@ async function run() {
           const id = req.params.id;
           const query = { _id: new ObjectId(id) };
           const result = await coffeeCollection.deleteOne(query);
+          res.send(result);
+      })
+
+
+      app.get('/users', async (req, res) => {
+          const result = await userCollection.find().toArray();
+          res.send(result);
+      })
+
+
+
+
+      app.post('/users', async (req, res) => {
+          const userProfile = req.body;
+          console.log(userProfile);
+          const result = await userCollection.insertOne(userProfile);
+          res.send(result);
+      })
+
+      app.delete('/users/:id', async (req, res) => {
+          const id = req.params.id;
+          const query = { _id: new ObjectId(id) };
+          const result = await userCollection.deleteOne(query);
           res.send(result);
       })
 
